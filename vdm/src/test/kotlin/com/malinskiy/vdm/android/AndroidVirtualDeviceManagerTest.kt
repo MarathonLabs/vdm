943e38a53649018b6ac924dfadcfc6e24b181023
Anton Malinskiy <anton@malinskiy.com>
2022-05-08 18:48:48 +1000

    fix(ci): fix AndroidVirtualDeviceManagerTest.testRead to skip if no devices present

--- vdm/src/test/kotlin/com/malinskiy/vdm/android/AndroidVirtualDeviceManagerTest.kt
+++ vdm/src/test/kotlin/com/malinskiy/vdm/android/AndroidVirtualDeviceManagerTest.kt
@@ -5,7 +5,9 @@
 package com.malinskiy.vdm.android
 
 import com.malinskiy.vdm.android.interactor.DiscoverAndroidHomeInteractor
+import com.malinskiy.vdm.device.VirtualDevice
 import org.junit.jupiter.api.Test
+import org.opentest4j.TestAbortedException
 
 class AndroidVirtualDeviceManagerTest {
 
@@ -19,7 +21,7 @@ class AndroidVirtualDeviceManagerTest {
 
     @Test
     fun testRead() {
-        val firstDevice = manager.browse().first()
+        val firstDevice: VirtualDevice = manager.browse().firstOrNull() ?: throw TestAbortedException("No avd devices present. Ignoring test")
         val virtualDevice = manager.read(firstDevice.id)
         println(virtualDevice)
     }
